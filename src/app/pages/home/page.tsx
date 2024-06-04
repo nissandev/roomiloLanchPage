c1fe6d0be21d2f4f6119562bc639479da6917aca
Md.Mosaraf Hossain Nissan <151007488+nissandev@users.noreply.github.com>
2024-06-04 18:35:42 +0600

    saved

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,17 +1,20 @@
+import dynamic from "next/dynamic";
+import React from "react";
 
-import dynamic from 'next/dynamic';
-import React from 'react';
-
-const Searchbar = dynamic(() => import('@/components/Searchbar/Searchbar'), { suspense: true });
-const Countdown = dynamic(() => import('@/components/countdown/Countdown'), { suspense: true });
+const Searchbar = dynamic(() => import("@/components/Searchbar/Searchbar"), {
+  suspense: true,
+});
+const Countdown = dynamic(() => import("@/components/countdown/Countdown"), {
+  suspense: true,
+});
 const Home = () => {
-    const releaseDate = new Date(process.env.NEXT_PUBLIC_OPENING_DATE || '');
-    return (
-        <div>
-            <Searchbar/>
-            {/* <Countdown releaseDate={releaseDate}/> */}
-        </div>
-    );
+  const releaseDate = new Date(process.env.NEXT_PUBLIC_OPENING_DATE || "");
+  return (
+    <div>
+      <Searchbar />
+      {/* <Countdown releaseDate={releaseDate}/> */}
+    </div>
+  );
 };
 
 export default Home;
