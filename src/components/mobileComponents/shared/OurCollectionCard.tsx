0ae3f932f01fc7d1c5e61a7ea4cce7325ca31c61
dipjitbaroijse <151007488+nissandev@users.noreply.github.com>
2024-06-11 15:21:11 +0600

    added overlay text

--- src/components/mobileComponents/shared/OurCollectionCard.tsx
+++ src/components/mobileComponents/shared/OurCollectionCard.tsx
@@ -7,88 +7,85 @@ import hotel_r4 from "../../../assets/hotelImages/hotel_r4.jpg";
 import hotel_r5 from "../../../assets/hotelImages/hotel_r1.jpg";
 import { FaRegStar } from "react-icons/fa";
 
-
-const OurCollectionCard:React.FC = () => {
+const OurCollectionCard: React.FC = () => {
   const recommendedHotel = [
     {
       hotelImg: hotel_r1,
-      location: "Dhaka, Bangladesh",
-      hotelName: "Hotel Nissan Paradise",
-      hotelTag:'Town House',
-      price:7247,
-      discountPrice:1767,
-      discount:20,
-      totalBooking:2000,
-      tex_fees:'+ 256 taxes & fees',
-      rating:4.6
+      serviceTitle: "Company Serviced",
+      hotelTag: "Premium",
+      serviceSubTitle: "Roomilo Service Premium",
+      typeOfPlace: "hotels",
+      overlayText:'company Service',
+      rating: 4.6,
     },
     {
-        hotelImg: hotel_r2,
-        location: "Dhaka, Bangladesh",
-        hotelName: "Hotel Cox's bazar Sea View",
-        hotelTag:'Collection O',
-        price:8247,
-        discountPrice:8067,
-        discount:10,
-        totalBooking:100,
-        rating:3.6,
-        tex_fees:'+ 256 taxes & fees'
-      },
-      {
-        hotelImg: hotel_r3,
-        location: "Dhaka, Bangladesh",
-        hotelName: "Hotel Nissan Paradise",
-        hotelTag:'Town House',
-        price:7247,
-        discountPrice:1767,
-        discount:20,
-        totalBooking:2000,
-        rating:4.6,
-        tex_fees:'+ 256 taxes & fees'
-      },
-      {
-        hotelImg: hotel_r4,
-        location: "Dhaka, Bangladesh",
-        hotelName: "Hotel Nissan Paradise",
-        hotelTag:'Town House',
-        price:7247,
-        discountPrice:1767,
-        discount:20,
-        totalBooking:2000,
-        rating:4.6,
-        tex_fees:'+ 256 taxes & fees'
-      },
-      {
-        hotelImg: hotel_r5,
-        location: "Dhaka, Bangladesh",
-        hotelName: "Hotel Nissan Paradise",
-        hotelTag:'Town House',
-        price:7247,
-        discountPrice:1767,
-        discount:20,
-        totalBooking:2000,
-        rating:4.6,
-        tex_fees:'+ 256 taxes & fees'
-      },
-
+      hotelImg: hotel_r2,
+      serviceTitle: "Roomilo Home",
+      hotelTag: "Relaxation",
+      serviceSubTitle: "Modern & Stylish",
+      typeOfPlace: "home stays",
+      overlayText:'Home',
+      rating: 4.6,
+    },
+    {
+      hotelImg: hotel_r3,
+      serviceTitle: "Collection O",
+      hotelTag: "Premium",
+      serviceSubTitle: "Premium Business",
+      overlayText:'Collection O',
+      typeOfPlace: "stays",
+      rating: 4.6,
+    },
+    {
+      hotelImg: hotel_r4,
+      serviceTitle: "Spot On",
+      hotelTag: "Town House",
+      serviceSubTitle: "Comfortable Budget",
+      overlayText:'Spot On',
+      typeOfPlace: "stays",
+      rating: 4.6,
+    },
+    {
+      hotelImg: hotel_r5,
+      serviceTitle: "Capital O",
+      hotelTag: "premium",
+      serviceSubTitle: "Beautiful Design",
+      overlayText:'C O',
+      typeOfPlace: "stays",
+      rating: 4.6,
+    },
+    {
+      hotelImg: hotel_r3,
+      serviceTitle: "Roomilo Townhouse",
+      hotelTag: "premium",
+      serviceSubTitle: "Chic stays at the most",
+      overlayText:'Townhouse',
+      typeOfPlace: "affordable stays",
+      rating: 4.6,
+    },
   ];
 
   return (
     <div className="px-[3.8%] py-[3.8%]">
       <div>
-        <h1 className="font-bold text-[18px] mt-[4%] mb-[3%]">Our Collections</h1>
+        <h1 className="font-bold text-[18px] mt-[4%] mb-[3%]">
+          Our Collections
+        </h1>
       </div>
       <div className="flex w-full px-2 gap-4 overflow-x-auto">
         {recommendedHotel?.map((recommended, ind) => (
           <div className="shrink-0" key={ind}>
             <div className="relative">
-              <Image
-                src={recommended?.hotelImg}
-                alt="hotel"
-                width={156}
-                height={96}
-                className="rounded-md object-cover"
-              />
+              <div className="relative">
+                <Image
+                  src={recommended?.hotelImg}
+                  alt="hotel"
+                  width={156}
+                  height={96}
+                  className="rounded-md object-cover"
+                />
+                <h3 className="h-full uppercase absolute bg-black/30 text-white top-0 w-full flex items-center justify-center text-[8px]">{recommended?.overlayText}</h3>
+              </div>
               <div className="absolute top-3 bg-yellow-600  h-[12px] w-[60px] rounded-[1px] -left-[4px] flex items-center justify-center">
                 <div className="bg-yellow-700 -z-10 r  rounded-r-none h-full w-[7%] top-[5px] absolute left-0"></div>
                 <div className="bg-white r  rounded-r-none h-full w-[7%] top-[8px] -z-10 absolute -left-[3px] -rotate-45"></div>
@@ -98,28 +95,28 @@ const OurCollectionCard:React.FC = () => {
               </div>
               <div className="absolute -bottom-1.5 bg-white border h-[12px] w-[60px] flex items-center justify-center rounded-sm left-2">
                 <p className="  text-[#f26600] text-center text-[6px] whitespace-nowrap  w-fit mx-auto gap-1 flex items-center">
-                 <span><FaRegStar/></span> <span>{recommended?.rating}</span> <span>({recommended?.totalBooking})</span>
+                  <span>
+                    <FaRegStar className="text-md -mt-[1.3px]" />
+                  </span>{" "}
+                  <span>{recommended?.rating} / 5</span>
                 </p>
               </div>
             </div>
             <div>
               <div className="leading-[16px] pt-5">
-                <h1 className="text-[14px] font-bold">
-                  {recommended?.hotelName?.length >24 ? `${recommended?.hotelName?.slice(0,20)}...`:recommended?.hotelName}
-                </h1>
-                <p className="text-[12px] text-gray-500">Dhaka, Bangladesh</p>
-              </div>
-              <div>
-                <div>
-                  <div className="flex gap-4 items-center text-[12px]">
-                    <h2 className="text-[16px] font-bold">$1767</h2>
-                    <p className="line-through">$7247</p>
-                    <p className="text-[#f26600]"> 72% off</p>
-                  </div>
-                  <p className="text-[12px] text-gray-500 tracking-widest">
-                    + 256 taxes & fees
-                  </p>
-                </div>
+                {recommended?.serviceTitle && (
+                  <h1 className="text-[14px] font-bold">
+                    {recommended.serviceTitle.length > 24
+                      ? `${recommended.serviceTitle.slice(0, 20)}...`
+                      : recommended.serviceTitle}
+                  </h1>
+                )}
+                <p className="text-[12px] text-gray-500">
+                  {recommended?.serviceSubTitle}
+                </p>
+                <p className="text-[12px] text-gray-500">
+                  {recommended?.typeOfPlace}
+                </p>
               </div>
             </div>
           </div>
