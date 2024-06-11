01cdf1fcd23fcbe880a8643a3ccbaeec001e2504
dipjitbaroijse <151007488+nissandev@users.noreply.github.com>
2024-06-11 16:45:32 +0600

    padding currection

--- src/components/mobileComponents/shared/CallToBook.tsx
+++ src/components/mobileComponents/shared/CallToBook.tsx
@@ -19,11 +19,11 @@ const CallToBook: React.FC = () => {
       };
   return (
     <div className="fixed bottom-24 right-3 z-10">
-      <a href="tel:01629880885" className={`text-[#f26600] border py-1 px-2 bg-white shadow-md ${!isInTop ? " w-full":'h-12 w-12 '} rounded-full overflow-hidden whitespace-nowrap flex shrink-0 items-center justify-center gap-2`}>
+      <a href="tel:01629880885" className={`text-[#f26600] border font-semibold py-1 px- bg-white shadow-md ${!isInTop ? " w-[141px] h-12":'h-12 w-12 '} rounded-full text-[12px] overflow-hidden whitespace-nowrap flex shrink-0 items-center justify-center gap-2`}>
         <p>
           <FiPhoneCall  className="text-sm"/>
         </p>
-        <p className={`${!isInTop? '' :'hidden'}`}>Call to book</p>
+        <p className={` tracking-widest  ${!isInTop? '' :'hidden'}`}>Call to book</p>
       </a>
     </div>
   );
